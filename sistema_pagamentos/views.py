36ce9936ae45df17cda24a24e3562191a4f4a40a
joaovitormessias <joaovitormessiasdamasio@gmail.com>
2025-03-17 11:03:22 -0300

    Criando primeira view clientes dentro de views

--- sistema_pagamentos/views.py
+++ sistema_pagamentos/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+# Usando o pacote http para renderizar um JSON quando chegar uma requisicao para determinada URL.
+from django.http import JsonResponse
 
-# Create your views here.
+# Views.
+def clientes(request):
+    if request.method == 'GET':
+        cliente = {'id': 1, 'nome':'Jão', 'CNPJ': 10000000001, 'email':'teste@gmail.com'}
+        return JsonResponse(cliente)
