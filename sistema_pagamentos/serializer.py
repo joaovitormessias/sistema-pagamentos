f8f4cb4e900ae9016e19d53298cf70c12d7e2d01
joaovitormessias <joaovitormessiasdamasio.com>
2025-04-26 09:39:44 -0300

    Serializador de dados.

--- sistema_pagamentos/serializer.py
+++ sistema_pagamentos/serializer.py
@@ -1,17 +1,37 @@
 from rest_framework import serializers
 from sistema_pagamentos.models import Cliente, Produto, Pedido, Pagamento, ItensPedido
 
+# -------------------------------- SERIALIZANDO DADOS -------------------------------- #
+
+
+
 class ClienteSerializer(serializers.ModelSerializer):
+
+    '''
+    Serializer para a entidade Cliente, transformando o modelo Cliente em um formato que pode ser facilmente
+    convertido para JSON ou outro formato.
+    '''
+
     class Meta:
         model = Cliente
         fields = '__all__'
 
 class ProdutoSerializer(serializers.ModelSerializer):
+
+    '''
+    Serializer para a entidade Produto, transformando o modelo Produto em um formato serializável.
+    '''
+
     class Meta:
         model = Produto
         fields = '__all__'
 
 class ItensPedidoSerializer(serializers.ModelSerializer):
+
+    '''
+    Serializer para a entidade ItensPedido, que inclui o cálculo do total do item (quantidade * preço).
+    '''
+
     total = serializers.SerializerMethodField()
     produto = ProdutoSerializer(read_only=True)
     class Meta:
@@ -19,9 +39,19 @@ class ItensPedidoSerializer(serializers.ModelSerializer):
         fields = ('produto', 'quantidade','total')
 
     def get_total(self, instance):
+
+        '''
+        Método para calcular o valor total de um item baseado na quantidade e no preço unitário do produto.
+        '''
+
         return instance.quantidade * instance.produto.preco_unitario
 
 class PedidoSerializer(serializers.ModelSerializer):
+
+    '''
+    Serializer para a entidade Pedido, que inclui os itens do pedido e informações do cliente.
+    '''
+
     itens = ItensPedidoSerializer(many=True)
     cliente = ClienteSerializer()
     
@@ -31,17 +61,32 @@ class PedidoSerializer(serializers.ModelSerializer):
         depth = 1
 
 class CriarEditarItensPedidoSerializer(serializers.ModelSerializer):
+
+    '''
+    Serializer para criação e edição dos itens de um pedido.
+    '''
+
     class Meta:
         model = ItensPedido
         fields = ('produto', 'quantidade')
 
 class CriarEditarPedidoSerializer(serializers.ModelSerializer):
+
+    '''
+    Serializer para criação e edição de um pedido, incluindo seus itens.
+    '''
+
     itens = CriarEditarItensPedidoSerializer(many=True)
     class Meta:
         model = Pedido
         fields = ('cliente','itens')      
 
     def create(self, validated_data):
+
+        '''
+        Criação de um pedido novo, incluindo seus itens.
+        '''
+
         itens = validated_data.pop('itens')
         pedido = Pedido.objects.create(**validated_data)
         for item in itens:
@@ -50,6 +95,11 @@ class CriarEditarPedidoSerializer(serializers.ModelSerializer):
         return pedido
     
     def update(self, instance, validated_data):
+
+        '''
+        Atualização de um pedido existente, incluindo a atualização de seus itens.
+        '''
+
         itens = validated_data.pop('itens')
         if itens:
             instance.itens.all()
@@ -61,6 +111,11 @@ class CriarEditarPedidoSerializer(serializers.ModelSerializer):
 
 
 class PagamentoSerializer(serializers.ModelSerializer):
+
+    '''
+    Serializer para a entidade Pagamento, incluindo informações do cliente e do pedido.
+    '''
+    
     cliente = ClienteSerializer(read_only=True)
     pedido = PedidoSerializer(read_only=True)
 
