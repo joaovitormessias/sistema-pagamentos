d73132bc3b4bb3a9b47fa37ccd18b659cdfb111c
joaovitormessias <joaovitormessiasdamasio.com>
2025-04-26 11:54:29 -0300

    Pagina de adminitração do django.

--- sistema_pagamentos/admin.py
+++ sistema_pagamentos/admin.py
@@ -1,41 +1,37 @@
+# Biblioteca do Django que permite a criação do admin do django, facilitando a visualização das nossas entidades.
 from django.contrib import admin
-from sistema_pagamentos.models import Cliente, Produto, Pedido, Pagamento, ItensPedido
 
-# -------------------------------- EXIBINDO INFORMAÇÕES NA PÁGINA DE ADMINSTRAÇÃO -------------------------------- #
+# Modelo das entidades da nossa base de dados.
+from sistema_pagamentos.models import Cliente, Produto, Pedido, Pagamento, ItensPedido 
 
-# Aqui é como vamos disponibilizar essas informações no meu admin: permitindo eu manipular os dados do meu banco de dados.
-
-#list_display = campos que serão exibidos na tabela.
-#list_display_links = links que me permitem exibir mais informações sobre.
-#search_fields = campos de pesquisa.
-#list_per_page = paginação.
-#model = referente a entidade do models.py.
-#inlines = permitem a edição de objetos relacionados diretamente na página de administração do objeto pai.
-
+# Register your models here.
 class Clientes(admin.ModelAdmin):
-    list_display = ('nome_cliente', 'cnpj', 'user') 
-    list_display_links = ( 'nome_cliente','cnpj',) 
-    search_fields = ('nome_cliente', 'cnpj',) 
-    list_per_page = 20 
+    list_display = ('nome_cliente', 'cnpj', 'user')
+    list_display_links = ( 'nome_cliente',)
+    search_fields = ('nome_cliente', 'cnpj',)
+    list_per_page = 20
 
-# Registrando essas informações no django admin
 admin.site.register(Cliente, Clientes)
 
 class Produtos(admin.ModelAdmin):
+    '''Exibe informações dos produtos contido na minha base de dados'''
+
     list_display = ('id_produto', 'nome_produto','descricao')
     list_display_links = ('id_produto','nome_produto')
     search_fields = ('id_produto', 'nome_produto',)
     list_per_page = 20
 
-# Registrando essas informações no django admin
 admin.site.register(Produto, Produtos)
 
 class ItensInline(admin.TabularInline):
+    '''Exibe os itens dentro do pedido realizado pelo cliente'''
+
     model = ItensPedido
 
-# Registrando essas informações no django admin
 @admin.register(Pedido)
 class PedidoAdmin(admin.ModelAdmin):
+    '''Exibe os pedidos realizados pelo meu cliente, isso inclui os itens dentro de cada pedido'''
+
     inlines = (ItensInline,)
     list_display = ('id_pedido', 'cliente')
     list_display_links = ('id_pedido','cliente')
@@ -44,10 +40,11 @@ class PedidoAdmin(admin.ModelAdmin):
 
 
 class Pagamentos(admin.ModelAdmin):
+    '''Exibe os pagamentos realizados dos clientes'''
+    
     list_display = ('id_pagamento', 'status_pagamento','data_pagamento')
     list_display_links = ('id_pagamento',)
     search_fields = ('id_pagamento',)
     list_per_page = 20
 
-# Registrando essas informações no django admin
 admin.site.register(Pagamento, Pagamentos)
