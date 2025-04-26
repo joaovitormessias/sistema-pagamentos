c219cb101ff01a662a81b23cae887a99e4da47ac
joaovitormessias <joaovitormessiasdamasio.com>
2025-04-26 11:29:45 -0300

    Configurações do projeto.

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -173,8 +174,8 @@ CELERY_BROKER_URL = 'amqp://guest:guest@localhost:5672//'
 # config email
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_PASSWORD = 'szhv ujva mlii edeq'
-EMAIL_PASSWORD = 'Jv$101511522'
-EMAIL_HOST_USER = 'joaovitormessias30@gmail.com'
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_PASSWORD = config('EMAIL_PASSWORD')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
